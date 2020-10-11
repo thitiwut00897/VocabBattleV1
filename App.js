6579a44d69425859ebfd1ac1ee60623499725e58
thitiwut <61070088@kmitl.ac.th>
2020-10-11 22:35:49 +0700

    Login/logout/firebase
    npm i firebase

--- App.js
+++ App.js
@@ -1,12 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, View, Button } from 'react-native';
+import Login from './screen/componant/login';
+import auth from './screen/firebase';
+import FirstScreen from './screen/main';
 
 export default function App() {
+  
+  const [session, setSession] = useState({
+    isLoggedIn: false,
+    currentUser: null,
+    errorMessage: null
+  });
+  useEffect(() => {
+    const handleAuth = auth.onAuthStateChanged(user => {
+      if (user) {
+        setSession({
+          isLoggedIn:true,
+          currentUser: user,
+          errorMessage:null
+        });
+      }
+    });
+
+    return () => {
+      handleAuth();
+    }
+  }, [])
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start wordfgdasfghasgasdgasdgsadgdsking on your app!</Text>
-      <StatusBar style="auto" />
+      {session.isLoggedIn ? 
+          <FirstScreen settt={setSession}/>
+         : <Login session={setSession}/>
+      } 
     </View>
   );
 }
