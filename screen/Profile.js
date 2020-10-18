4f873bc112ef2088c035c099b9712ff1ee82eafb
ploidplanp <waoekpr@gmail.com>
2020-10-18 16:08:16 +0700

    create 2 screens and navigation file

---
+++ screen/Profile.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View } from 'react-native';
+
+export default function Profile() {
+    return (
+        <View>
+
+        </View>
+    )
+}
+
+const style = StyleSheet.create({
+
+});
