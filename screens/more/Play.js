979ea248070762a56b52436adf934add63ce7b6c
thitiwut <61070088@kmitl.ac.th>
2020-11-08 14:13:28 +0700

    Update Play.js

--- screens/more/Play.js
+++ screens/more/Play.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { View, StyleSheet, Text, addons, TouchableOpacity } from 'react-native'
 import { TextInput } from 'react-native-gesture-handler';
 // import wordd from '../words_dictionary.json'
@@ -16,25 +16,37 @@ export default function Play() {
                 setHpmonster(hpmonster-(anwser.length*10))
                 setAnwser('')
             }else{
-                console.log(ผิด);
+                setAnwser('')
+                console.log('ผิด');
             }
         }
         
     }
-    const Checklog =()=>{
+
+    useEffect(()=>{
         if (hpmonster <= 0){
             // show status monster animation
             setHpmonster((Math.floor(Math.random()*4)+1)*50)
             setround(round+1)
             setAlphabet(String.fromCharCode(Math.floor(Math.random() * 26) + 97))
         }
-        return (<Text>hp monster is {hpmonster}</Text>);
-    }
+    })
+
+    // const Checklog =()=>{
+    //     // if (hpmonster <= 0){
+    //     //     // show status monster animation
+    //     //     setHpmonster((Math.floor(Math.random()*4)+1)*50)
+    //     //     setround(round+1)
+    //     //     setAlphabet(String.fromCharCode(Math.floor(Math.random() * 26) + 97))
+    //     // }
+    //     return (<Text>hp monster is {hpmonster}</Text>);
+    // }
     return(
         <View style={styles.container}>
             <Text>Play Screen</Text>
             <Text>round : {round}</Text>
-            <Checklog/>
+            {/* <Checklog/> */}
+            <Text>hp monster is {hpmonster}</Text>
             <Text>type : {alphabet}</Text>
             <TextInput style={{ height: 40, borderColor: 'gray', borderWidth: 1 }} autoCapitalize = 'none' onfocus="" onChangeText={(text)=>{setAnwser(text.toLowerCase())}}>{anwser}</TextInput>
             <TouchableOpacity onPress={() => {Check()}}><Text>submit</Text></TouchableOpacity> 
